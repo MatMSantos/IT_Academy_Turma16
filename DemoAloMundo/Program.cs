9649ee08c75136066e9bfe9c89cccab7f8a7981f
MatMSantos <fredsteammember@hotmail.com>
2022-07-19 15:08:10 -0300

    first commit

---
+++ DemoAloMundo/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Classe main é implícita aqui, não é necessário
+// Lembrando que main tem que ser único no projeto
+
+// using System é implícito aqui também, por isso posso usar Console diretamente
+// String? : tipo nullable - compilador faz certas análises de dados para evitar problemas com Null
+Console.WriteLine("Hello, World!");
